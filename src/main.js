8f2f95b42238f2ae16b7f60ff8a71229c84c843d
Adrian Messado <adrian.messado@gmail.com>
2018-05-29 06:50:43 -0700

    add links and input to header, remove extra unused sections

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
 // import { function } from './AgeConvertor';
+import './styles.css';
 // import 'bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import './styles.css';
+
 // import $ from 'jquery';
 //
 // $(document).ready(function() {
